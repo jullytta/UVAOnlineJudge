0cd84637345fb3000f96e43d2a742c4312adafb5
jullytta <jullyanavycas@gmail.com>
2015-06-20 13:31:15 -0300

    Problem 10263 AC

--- 10263.cpp
+++ 10263.cpp
@@ -1,48 +1,89 @@
 /*
 	Code for 10263 - Railway
 	Language: C++
-	Runtime: ?
+	Runtime: 0.000
 	Author: Jullytta
-	Submitted: ?
+	Submitted: 2015-06-20 16:29:59
 */
 
 #include <iostream>
+#include <limits>
+#include <cmath>
+#include <stdio.h>
 
 using namespace std;
 
 struct Point{
-	float x, y;
+	double x, y;
 };
 
-Point createPoint(float x, float y);
+Point createPoint(double x, double y);
+double distanceToPoint(Point a, Point b, Point m, Point *closestPoint);
+double distanceBetweenPoints(Point a, Point b);
+double dotProduct(Point a, Point b);
 
 int main(void){
 
 	int n;
-	float x, y;
-	Point m, bestPlace, a, b;
+	double x, y, minDistance, distance;
+	Point m, a, b, p, closestPoint;
 
 	while(cin >> x >> y >> n){
 
 		m = createPoint(x,y);
+		minDistance = numeric_limits<double>::max();
 
-		for(int i = 0; i < n+1; i++){
+		cin >> x >> y;
+		b = createPoint(x,y);
+		for(int i = 0; i < n; i++){
+			a = b;
 			cin >> x >> y;
-			a = createPoint(x,y);
-//https://en.wikipedia.org/wiki/Distance_from_a_point_to_a_line
+			b = createPoint(x,y);
+			distance = distanceToPoint(a, b, m, &p);
+			if(distance < minDistance){
+				minDistance = distance;
+				closestPoint = p;
+			}
 		}
 
-	}
+		printf("%.4f\n%.4f\n", closestPoint.x, closestPoint.y);
 
-	cout << "Hi there!\n";
+	}
 
 	return 0;
 
 }
 
-Point createPoint(float x, float y){
+Point createPoint(double x, double y){
 	Point m;
 	m.x = x;
 	m.y = y;
 	return m;
+}
+
+double distanceToPoint(Point a, Point b, Point m, Point *closestPoint){
+
+	double t, lengthSquared;
+	lengthSquared = (b.y-a.y)*(b.y-a.y)+(b.x-a.x)*(b.x-a.x);
+	t = dotProduct(createPoint(m.x-a.x,m.y-a.y), createPoint(b.x-a.x,b.y-a.y))/lengthSquared;
+	if(t < 0.0){
+		*closestPoint = a;
+		return distanceBetweenPoints(m, a);
+	}
+	else if(t > 1.0){
+		*closestPoint = b;
+		return distanceBetweenPoints(m, b);
+	}
+	Point projection = createPoint(a.x + t*(b.x-a.x), a.y + t*(b.y-a.y));
+	*closestPoint = projection;
+	return distanceBetweenPoints(m, projection);
+
+}
+
+double distanceBetweenPoints(Point a, Point b){
+	return sqrt((b.y-a.y)*(b.y-a.y)+(b.x-a.x)*(b.x-a.x));
+}
+
+double dotProduct(Point a, Point b){
+	return (a.x*b.x + a.y*b.y);
 }
